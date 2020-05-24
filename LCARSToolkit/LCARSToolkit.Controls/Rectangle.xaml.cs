a015a3e5358ea7de3fb8c76755c92a01be5badad
Matthes <matthes.koch@outlook.com>
2020-05-24 12:46:45 +0200

    Updated rectangle dependency property and event registration

--- LCARSToolkit/LCARSToolkit.Controls/Rectangle.xaml.cs
+++ LCARSToolkit/LCARSToolkit.Controls/Rectangle.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
@@ -8,13 +9,27 @@ namespace LCARSToolkit.Controls
 {
     public sealed partial class Rectangle : UserControl
     {
+
+        public Rectangle()
+        {
+            this.InitializeComponent();
+            Extensions.FlashTimer.Tick += FlashTimer_Tick;
+            Unloaded += (s, e) => { Extensions.FlashTimer.Tick -= FlashTimer_Tick; };
+        }
+
+        private void FlashTimer_Tick(object sender, object e)
+        {
+            IsLit = (Illumination == Illumination.Flashing) ? !IsLit : (Illumination == Illumination.On);
+        }
+
         public Brush Fill
         {
-            get { return rect.Fill; }
-            set { SetValue(FillProperty, value); rect.Fill = value; }
+            get { return (Brush)GetValue(FillProperty); }
+            set { SetValue(FillProperty, value);}
         }
-        public static readonly DependencyProperty FillProperty = DependencyProperty.Register(nameof(Fill), typeof(Brush), typeof(Rectangle), new PropertyMetadata(null));
 
+        public static readonly DependencyProperty FillProperty = 
+            DependencyProperty.Register(nameof(Fill), typeof(Brush), typeof(Rectangle), new PropertyMetadata(null));
 
         private bool _IsLit;
         public bool IsLit
@@ -38,13 +53,11 @@ namespace LCARSToolkit.Controls
             get { return (Illumination)GetValue(IlluminationProperty); }
             set { SetValue(IlluminationProperty, value); }
         }
-        public static readonly DependencyProperty IlluminationProperty = DependencyProperty.Register("Illumination", typeof(Illumination), typeof(Rectangle), new PropertyMetadata(Illumination.On));
+
+        public static readonly DependencyProperty IlluminationProperty = DependencyProperty.Register("Illumination", 
+            typeof(Illumination), typeof(Rectangle), new PropertyMetadata(Illumination.On));
 
 
-        public Rectangle()
-        {
-            this.InitializeComponent();
-            Extensions.FlashTimer.Tick += delegate { IsLit = (Illumination == Illumination.Flashing) ? !IsLit : (Illumination == Illumination.On); };
-        }
+
     }
 }
